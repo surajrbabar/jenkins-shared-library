b9846606c1c3e92486e352e80fc089935c891ed1
Suraj Babar <surajrbabar@gmail.com>
2025-07-28 12:33:21 +0530

    Update buildImage.groovy
    Added the parameter for docker image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/user/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo "building the docker image ..."
     withCredentials([usernamePassword(credentialsId : 'dockerhub-credentials', usernameVariable : 'USER', passwordVariable : 'PASS')]){
-        sh 'docker build -t surajrbabar/java-maven-app:jma-1.2 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push surajrbabar/java-maven-app:jma-1.2'
+        sh "docker push $imageName"
     }
 }
